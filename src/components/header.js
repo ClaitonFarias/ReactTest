7e949e0c8888ba66ae86aad57c9048a95a2cf210
ClaitonFarias <claiton.trisch@gmail.com>
2024-05-24 21:15:03 -0300

    ajustes

--- src/components/header.js
+++ src/components/header.js
@@ -24,7 +24,7 @@ function Header() {
               */}
             </IconButton>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Gerenciamento de Projetos
+              Realizando pequenas edições no template
             </Typography>
           </Toolbar>
         </AppBar>
